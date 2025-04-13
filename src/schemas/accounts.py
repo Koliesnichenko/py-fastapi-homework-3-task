b4d18a2b7fb9a3a7b1dbdf68f54077f193725461
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-04-13 23:11:15 +0300

    solution

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,52 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 
 from database import accounts_validators
 
 
-# Write your code here
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("email", mode="after")
+    @classmethod
+    def validate_email(cls, value: str) -> str:
+        return accounts_validators.validate_email(value)
+
+    @field_validator("password", mode="after")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        return accounts_validators.validate_password(value)
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+    password: str
+
+
+class UserLoginRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    refresh_token: str
